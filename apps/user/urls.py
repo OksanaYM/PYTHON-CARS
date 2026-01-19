3a9ae2afd2c4ff92da672d0d56b60853408303f4
Oksana <oksanakysylychak@gmail.com>
2026-01-19 09:31:16 +0200

    CW

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from apps.user.views import UserListCreateView
+from apps.user.views import UserListCreateView, BlockUserView, UnblockUserView, UserToAdminView
 
 urlpatterns = [
     path('', UserListCreateView.as_view(), name='user_list_create'),
+    path('/<int:pk>/block', BlockUserView.as_view(), name='user_block'),
+    path('/<int:pk>/unblock', UnblockUserView.as_view(), name='user_unblock'),
+    path('/<int:pk>/to_admin', UserToAdminView.as_view(), name='user_to_admin'),
 
 ]
